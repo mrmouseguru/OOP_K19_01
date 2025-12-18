9267c649b506d26294cabe792c8ab4d8e9733329
CGM <mrmouse.guru@gmail.com>
2025-12-18 10:32:12 +0700

    CLASS MULTIPLIER (PROVIDER)

---
+++ src/oop_k19_1/provider/Multiplier.java
@@ -0,0 +1,29 @@
+package oop_k19_1.provider;
+
+/**
+ *
+ * @author phuoc
+ */
+public class Multiplier {
+    private int number1;
+    private int number2;
+    
+    public Multiplier(){}
+    
+    public Multiplier(int n1, int n2)
+    {
+        number1 = n1;
+        number2 = n2;
+    }
+    
+    public int multiply(){
+        return number1 * number2;
+    }
+    
+    //getters
+    //setters
+    
+    
+    
+    
+}
